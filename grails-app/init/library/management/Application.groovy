0fb408c526e2148a2d0f4c57bfcfb0e9179bc8d2
vishaltyagi13 <vishal.tyagi@perfios.com>
2022-10-04 12:56:29 +0530

    added library-management project

---
+++ grails-app/init/library/management/Application.groovy
@@ -0,0 +1,10 @@
+package library.management
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
